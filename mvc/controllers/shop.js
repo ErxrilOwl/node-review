5fd25d7501cf7ca19793b3ec7112798fd1da7de2
Nikko <atuan.nikko@gmail.com>
2025-12-11 22:30:18 +0800

    feat: mvc - store order items

--- mvc/controllers/shop.js
+++ mvc/controllers/shop.js
@@ -1,5 +1,5 @@
 const Product = require('../models/product');
-const Cart = require('../models/cart');
+const Order = require('../models/order');
 
 exports.getProducts = (req, res, next) => {
   Product.findAll()
@@ -127,6 +127,36 @@ exports.postCartDeleteProduct = (req, res, next) => {
     })
     .catch(err => console.log(err));
 };
+
+exports.postOrder = (req, res, next) => {
+  let fetchedCart;
+
+  req.user.getCart()
+    .then(cart => {
+      fetchedCart = cart;
+      return cart.getProducts();
+    })
+    .then(products => {
+      console.log(products);
+      return req.urder.createOrder()
+        .then(order => {
+          order.addProducts(products.map(product => {
+            product.orderItem = {
+              quantity: product.cartItem.quantity
+            }
+            return product;
+          }));
+        })
+        .catch(err => console.log(err));
+    })
+    .then(result => {
+      return fetchedCart.setProducts(null);
+    })
+    .then(result => {
+      res.redirect('/orders');
+    })
+    .catch(err => console.log(err));
+}
 
 exports.getOrders = (req, res, next) => {
     res.render('shop/orders', {
