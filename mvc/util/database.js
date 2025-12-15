734479474f18a6320d7b41902081f573969d62c0
Nikko <atuan.nikko@gmail.com>
2025-12-15 14:47:10 +0800

    feat: mvc - update to mongo db

--- mvc/util/database.js
+++ mvc/util/database.js
@@ -9,10 +9,26 @@
 
 // module.exports = pool.promise();
 
-const { Sequelize } = require('sequelize');
-const sequelize = new Sequelize('node_complete', 'root', '', {
-    dialect: 'mysql',
-    host: 'localhost'
-});
+// const { Sequelize } = require('sequelize');
+// const sequelize = new Sequelize('node_complete', 'root', '', {
+//     dialect: 'mysql',
+//     host: 'localhost'
+// });
 
-module.exports = sequelize;
+// module.exports = sequelize;
+
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
+
+const mongoConnect = (callback) => {
+    MongoClient.connect(
+        'mongodb+srv://node_db_user:XVXUqEbXz00ABF0I@node-review-cl.4lyttfg.mongodb.net/?appName=node-review-cl'
+    ).then(client => {
+        console.log('Connected!');
+        callback(client);
+    }).catch(err => {
+        console.log(err);
+    });
+};
+
+module.exports = mongoConnect;
