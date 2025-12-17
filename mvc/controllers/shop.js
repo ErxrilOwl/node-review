74ea62938ceddb7feeaa4a22aef00fa6024a80f4
Nikko <atuan.nikko@gmail.com>
2025-12-17 21:59:41 +0800

    feat: mvc - display cart items

--- mvc/controllers/shop.js
+++ mvc/controllers/shop.js
@@ -45,37 +45,16 @@ exports.getIndex = (req, res, next) => {
 
 exports.getCart = (req, res, next) => {
   req.user.getCart()
-    .then(cart => {
-      console.log(cart);
-      return cart.getProducts().then(products => {
-        res.render('shop/cart', {
-          path: '/cart',
-          pageTitle: 'Your Cart',
-          products: products
-        });
-      }).catch(err => console.log(err));
+    .then(products => {
+      res.render('shop/cart', {
+        path: '/cart',
+        pageTitle: 'Your Cart',
+        products: products
+      });
     })
     .catch(err => {
       console.log(err);
     });
-
-  // Cart.getCart(cart => {
-  //   Product.fetchAll(products => {
-  //     const cartProducts = [];
-  //     for (let product of products) {
-  //       const cartProductData = cart.products.find(prod => prod.id === product.id);
-  //       if (cartProductData) {
-  //         cartProducts.push({productData: product, qty: cartProductData.qty});
-  //       }
-  //     }
-  //     res.render('shop/cart', {
-  //       path: '/cart',
-  //       pageTitle: 'Your Cart',
-  //       products: cartProducts
-  //     });
-  //   });
-  // });
-    
 }
 
 exports.postCart = (req, res, next) => {
@@ -86,6 +65,7 @@ exports.postCart = (req, res, next) => {
       })
       .then(result => {
         console.log(result);
+        res.redirect('/cart');
       })
       .catch(err => console.log(err));
 
