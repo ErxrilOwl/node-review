6bd93ca522db798844edf8f3d0567ee9b66ee0ab
Nikko <atuan.nikko@gmail.com>
2025-12-26 21:01:32 +0800

    feat: mvc - Delete Product

--- mvc/controllers/admin.js
+++ mvc/controllers/admin.js
@@ -83,7 +83,8 @@ exports.getProducts = (req, res, next) => {
 
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.deleteById(prodId)
+  
+  Product.findByIdAndDelete(prodId)
     .then(() => {
       res.redirect("/admin/products");
     })
