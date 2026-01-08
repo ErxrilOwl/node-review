eef13ab13cab1452bc066199b73cc12dc47a324c
Nikko <atuan.nikko@gmail.com>
2026-01-08 21:49:43 +0800

    feat: added mongodb session

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
 
 const errorController = require('./controllers/error');
 
@@ -13,6 +14,10 @@ const User = require('./models/user');
 dotenv.config();
 
 const app = express();
+const store = new MongoDBStore({
+    uri: process.env.MONGODB_URI,
+    collection: 'sessions'
+});
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -26,7 +31,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(session({
     secret: process.env.SESSION_SECRET,
     resave: false,
-    saveUninitialized: false
+    saveUninitialized: false,
+    store: store
 }))
 
 app.use((req, res, next) => {
