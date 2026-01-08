d6bc88d2cdb634c6c53ac60a02eaf5ff519937bf
Nikko <atuan.nikko@gmail.com>
2026-01-08 21:28:53 +0800

    feat: added session middleware

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const session = require('express-session');
 
 const errorController = require('./controllers/error');
 
@@ -22,6 +23,11 @@ const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false
+}))
 
 app.use((req, res, next) => {
     User.findById('695ba5442ee704667a07e0a2')
