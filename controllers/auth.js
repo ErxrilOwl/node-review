7c40e3e831adb3a5b2ce0d19692cc25aedcb0b72
Nikko <atuan.nikko@gmail.com>
2026-01-13 21:20:12 +0800

    feat: signin

--- controllers/auth.js
+++ controllers/auth.js
@@ -18,15 +18,32 @@ exports.getSignup = (req, res, next) => {
 };
 
 exports.postLogin = (req, res, next) => {
-  User.findById('6964f7a6ec6fe6481416fbbb')
-      .then(user => {
-          req.session.isLoggedIn = true;
-          req.session.user = user;
-          req.session.save(() => {
-            res.redirect('/');
-          });
-      })
-      .catch(err => console.log('Error Login: ', err));
+  const email = req.body.email;
+  const password = req.body.password;
+
+  User.findOne({
+    email: email
+  })
+    .then(user => {
+      if (!user) {
+        return res.redirect('/login');
+      }
+
+      bcrypt.compare(password, user.password)
+        .then((doMatch) => {
+          if (doMatch) {
+            req.session.isLoggedIn = true;
+            req.session.user = user;
+            return req.session.save(() => {
+              res.redirect('/');
+            });
+          }
+          res.redirect('/login');
+        })
+        .catch(err => console.log(err));
+    
+    })
+    .catch(err => console.log('Error Login: ', err));
 };
 
 exports.postSignup = (req, res, next) => {
@@ -41,18 +58,18 @@ exports.postSignup = (req, res, next) => {
     if (userDoc) {
       return res.redirect('/signup');
     }
-    return bcrypt.hash(password, 12);
-  })
-  .then(hashedPassword => {
-    const user = new User({
-      email: email,
-      password: hashedPassword,
-      cart: { items: [] }
-    });
-    user.save();
-  })
-  .then(result => {
-    res.redirect('/login');
+    return bcrypt.hash(password, 12)
+      .then(hashedPassword => {
+        const user = new User({
+          email: email,
+          password: hashedPassword,
+          cart: { items: [] }
+        });
+        user.save();
+      })
+      .then(result => {
+        res.redirect('/login');
+      })
   })
   .catch(err => console.log(err))
 };
