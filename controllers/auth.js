f5df9c19981570931d9855e651b822452eea0ffe
Nikko <atuan.nikko@gmail.com>
2026-01-14 21:25:19 +0800

    feat: added email sender

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,16 @@
 const bcrypt = require('bcryptjs');
 const User = require('../models/user');
+const nodemailer = require('nodemailer');
+
+// Looking to send emails in production? Check out our Email API/SMTP product!
+const transport = nodemailer.createTransport({
+  host: process.env.MAIL_HOST,
+  port: 2525,
+  auth: {
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASSWORD
+  }
+});
 
 exports.getLogin = (req, res, next) => {
   let message = req.flash('error');
@@ -84,7 +95,14 @@ exports.postSignup = (req, res, next) => {
       })
       .then(result => {
         res.redirect('/login');
+        return transport.sendMail({
+          to: email,
+          from: 'shop@test.com',
+          subject: 'Signup succeeded',
+          html: '<h1>You successfully signed up!</h1>'
+        })
       })
+      .catch(err => console.log(err))
   })
   .catch(err => console.log(err))
 };
