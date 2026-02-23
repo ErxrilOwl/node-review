a1bef415fe3a9633409c8415e1eb2efeedbf7209
Nikko <atuan.nikko@gmail.com>
2026-02-23 21:03:48 +0800

    feat: express validator

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,6 +4,8 @@ const bcrypt = require('bcryptjs');
 const User = require('../models/user');
 const nodemailer = require('nodemailer');
 
+const { validationResult } = require('express-validator');
+
 // Looking to send emails in production? Check out our Email API/SMTP product!
 const transport = nodemailer.createTransport({
   host: process.env.MAIL_HOST,
@@ -77,6 +79,16 @@ exports.postSignup = (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
   const confirmPassword = req.body.confirmPassword;
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    console.log(errors.array())
+    return res.status(422).render('auth/signup', {
+    path: '/signup',
+    pageTitle: 'Signup',
+    errorMessage: errors.array()[0].msg
+  });
+  }
 
   User.findOne({
     email: email
