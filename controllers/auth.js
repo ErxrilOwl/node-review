61bf8563e3e2f6a3119f6dac6380b1b4cb0e2021
Nikko <atuan.nikko@gmail.com>
2026-03-09 22:08:18 +0800

    feat: login validation

--- controllers/auth.js
+++ controllers/auth.js
@@ -26,7 +26,12 @@ exports.getLogin = (req, res, next) => {
   res.render("auth/login", {
     path: "/login",
     pageTitle: "Login",
-    errorMessage: message
+    errorMessage: message,
+    oldInput: {
+      email: '',
+      password: ''
+    },
+    validationErrors: []
   });
 };
 
@@ -58,7 +63,12 @@ exports.postLogin = (req, res, next) => {
     return res.status(422).render('auth/login', {
       path: '/login',
       pageTitle: 'Login',
-      errorMessage: errors.array()[0].msg
+      errorMessage: errors.array()[0].msg,
+      oldInput: {
+        email: email,
+        password: password
+      },
+      validationErrors: errors.array()
     })
   }
 
@@ -67,8 +77,16 @@ exports.postLogin = (req, res, next) => {
   })
     .then(user => {
       if (!user) {
-        req.flash('error', 'Invalid credentials.');
-        return res.redirect('/login');
+        return res.status(422).render('auth/login', {
+          path: '/login',
+          pageTitle: 'Login',
+          errorMessage: 'Invalid credentials',
+          oldInput: {
+            email: email,
+            password: password
+          },
+          validationErrors: []
+        })
       }
 
       bcrypt.compare(password, user.password)
@@ -80,8 +98,16 @@ exports.postLogin = (req, res, next) => {
               res.redirect('/');
             });
           }
-          req.flash('error', 'Invalid credentials.');
-          res.redirect('/login');
+          return res.status(422).render('auth/login', {
+            path: '/login',
+            pageTitle: 'Login',
+            errorMessage: 'Invalid credentials',
+            oldInput: {
+              email: email,
+              password: password
+            },
+            validationErrors: []
+          })
         })
         .catch(err => console.log(err));
     
