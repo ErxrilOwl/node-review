910bdcbd7f9241f977709e1579e02e1f9c39852f
Nikko <atuan.nikko@gmail.com>
2026-03-18 21:42:21 +0800

    feat: file image view

--- app.js
+++ app.js
@@ -39,6 +39,7 @@ app.set('views', 'views');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(multer({ storage: fileStorage, fileFilter }).single('image'));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
 mongoose.connect(process.env.MONGODB_URI)
     .then(() => {
