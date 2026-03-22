34cb66dd1192d82468e539fe6bef5c17fe4867b7
Nikko <atuan.nikko@gmail.com>
2026-03-22 21:18:49 +0800

    feat: added delete product request

--- controllers/admin.js
+++ controllers/admin.js
@@ -165,8 +165,9 @@ exports.getProducts = (req, res, next) => {
     .catch(err => res.redirect('/500'));
 };
 
-exports.postDeleteProduct = (req, res, next) => {
-  const prodId = req.body.productId;
+exports.deleteProduct = (req, res, next) => {
+  const prodId = req.params.productId;
+
   Product.findById(prodId)
     .then(product => {
       if (!product) {
@@ -175,7 +176,13 @@ exports.postDeleteProduct = (req, res, next) => {
       fileHelper.deleteFile();
       return Product.deleteOne({ _id: prodId, userId: req.user._id });
     })
-    .then(() => res.redirect('/admin/products'))
-    .catch(err => res.redirect('/500'));
+    .then(() => {
+      res.status(200).json({
+        message: 'Success!'
+      });
+    })
+    .catch(err => {
+      res.status(500).json({ message: 'Deleting product failed.' });
+    });
 
 };
--- public/js/admin.js
+++ public/js/admin.js
@@ -3,4 +3,19 @@ const deleteProduct = (btn) => {
     const prodId = btn.parentNode.querySelector('[name=productId]');
     const csrf = btn.parentNode.querySelector('[name=_csrf]');
 
+    const productElement = btn.closest('article');
+
+    fetch('/admin/product/' + prodId, {
+        method: 'DELETE',
+        headers: {
+            'csrf-token': csrf
+        }
+    }).then(res => {
+        return res.json();
+    }).then(data => {
+        console.log(data);
+        productElement.parentNode().removeChild(productElement);
+    }).catch(err => {
+        console.log(err);
+    });
 }
